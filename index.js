3ede3a890ad97469c0863e24cd83b0688e02804d
abhishek <alabhishek17@gmail.com>
2024-08-01 20:12:25 +0530

    file is added

--- index.js
+++ index.js
@@ -32,5 +32,9 @@ mongoose
 app.use("/api/v1/user",UserRouter)
 app.use("/api/v1/post",Postrouter)
 
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/index.html")
+})
+
 const PORT = 7777;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
